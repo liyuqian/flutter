412ea5c0dfce9a5169fd5a02adfa1ae5320ff877
Pierre-Louis <6655696+guidezpl@users.noreply.github.com>
2020-06-29 10:05:22 +0200

    Update new gallery HEAD commit
    The gallery repository history has been rewritten (see https://github.com/flutter/gallery/issues/87), so updating to HEAD commit

--- dev/devicelab/lib/tasks/new_gallery.dart
+++ dev/devicelab/lib/tasks/new_gallery.dart
@@ -19,7 +19,7 @@ class NewGalleryPerfTest extends PerfTest {
     // Manually roll the new gallery version for now. If the new gallery repo
     // turns out to be updated frequently in the future, we can set up an auto
     // roller to update this version.
-    await getNewGallery('59489e5571ddf554fc62ef52e9b16f1fed291026', galleryDir);
+    await getNewGallery('d00362e6bdd0f9b30bba337c358b9e4a6e4ca950', galleryDir);
     return await super.run();
   }
 
